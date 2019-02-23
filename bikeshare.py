739d5fac485ef13553cdbdb2a58aa8915cbbf189
Emily Lim <lim.emilyb@gmail.com>
2019-02-23 14:19:59 -0800

    removed greeting prior to user input

--- bikeshare.py
+++ bikeshare.py
@@ -18,7 +18,6 @@ def get_filters():
     city = None
     month = None
     day = None
-    print('Hello! Let\'s explore some US bikeshare data!')
     while day == None:
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid input
             city = input("Please enter a city name (either Chicago, New York City, or Washington): ")
@@ -29,7 +28,7 @@ def get_filters():
                 if city == 'chicago' or city == 'new york city' or city =='washington':
                     print('\n')
                     break
-              
+
             month = input("Please enter a month between January and June, or enter 'all' to see data for all months: ")
             month = month.lower()
             while month != 'all' and month != 'january' and month != 'february' and month != 'march' and month != 'april' and month != 'may' and month !=                 'june':
@@ -38,7 +37,7 @@ def get_filters():
                 if month == 'all' or month == 'january' or month == 'february' or month == 'march' or month == 'april' or month == 'may' or month ==                           'june':
                     print('\n')
                     break
-                    
+
             day = input("Please enter a day of the week, or enter 'all' to see data for all days: ")
             day = day.lower()
             while day != 'all' and day != 'monday' and day != 'tuesday' and day != 'wednesday' and day != 'thursday' and day != 'friday' and day !=                       'saturday' and day != 'sunday':
@@ -47,12 +46,12 @@ def get_filters():
                 if day == 'all' or day == 'monday' or day == 'tuesday' or day == 'wednesday' or day == 'thursday' or day == 'friday' or day ==                                 'saturday' or day == 'sunday':
                     print('\n')
                     break
-               
+
     # TO DO: get user input for month (all, january, february, ... , june)
 
 
     # TO DO: get user input for day of week (all, monday, tuesday, ... sunday)
-    
+
     print('-'*40)
     return city, month, day
 
@@ -111,11 +110,11 @@ def time_stats(df):
     popular_month = df['month'].mode()[0]
     name = months.get(popular_month)
     print("Most common month: ", name)
-  
+
     # TO DO: display the most common day of week
     popular_day = df['day_of_week'].mode()[0]
     print("Most common day of the week: ", popular_day)
-    
+
     # TO DO: display the most common start hour
     df['Start Time'] = pd.to_datetime(df['Start Time'])
 
@@ -139,7 +138,7 @@ def station_stats(df):
     # TO DO: display most commonly used start station
     popular_start_station = df['Start Station'].mode()[0]
     print("Most common start station is: ", popular_start_station)
-    
+
     # TO DO: display most commonly used end station
     popular_end_station = df['End Station'].mode()[0]
     print("Most common end station is: ", popular_end_station)
@@ -180,10 +179,10 @@ def user_stats(df):
     user_types = df['User Type'].value_counts()
     print("User type counts: ")
     print(user_types)
-    
+
     if 'Gender' not in df:
         print("\nThere is no data on gender or birth year for this city.")
-        
+
     else:
         # TO DO: Display counts of gender
         df_cleanup = df.dropna(subset = ['Gender'])
@@ -197,15 +196,15 @@ def user_stats(df):
         min_birthyear = df_birthyear_cleanup['Birth Year'].min()
         print('\n')
         print("Earliest birth year: ", int(min_birthyear))
-    
+
         max_birthyear = df_birthyear_cleanup['Birth Year'].max()
         print("Most recent birth year: ", int(max_birthyear))
-    
+
         mode_birthyear = df_birthyear_cleanup['Birth Year'].mode()
         print("Most common year of birth: ", int(mode_birthyear))
-    
+
         print("\nThis took %s seconds." % (time.time() - start_time))
-        
+
     print('-'*40)
 
 def raw_data(df):
@@ -215,12 +214,12 @@ def raw_data(df):
     user_response = None
     user_response = input("Would you like to see five lines of raw data from this data set? ")
     user_response = user_response.lower()
-    
+
     if user_response != 'yes' and user_response != 'no':
         user_response = input("This is not a valid response.  Please enter 'yes' or 'no'. ")
         user_response = user_response.lower()
-   
-            
+
+
     while user_response == 'yes':
         print(df[x:y])
         user_response = input("Would you like to see the next five lines of raw data from this data set? ")
@@ -234,23 +233,23 @@ def raw_data(df):
             if user_response != 'yes' and user_response != 'no':
                 user_response = input("This is not a valid response.  Please enter 'yes' or 'no'. ")
                 user_response = user_response.lower()
-            else: 
+            else:
                 print('Done')
-               
+
     print('-'*40)
-    
+
 def main():
     while True:
         city, month, day = get_filters()
         df = load_data(city, month, day)
-       
+
 
         time_stats(df)
         station_stats(df)
         trip_duration_stats(df)
         user_stats(df)
         raw_data(df)
-        
+
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
